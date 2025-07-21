e533aa11aa4d58e22ff6f46c2558e311607d0a78
wisdomeffiong2008@gmail.com <wisdomeffiong2008@gmail.com>
2025-07-21 15:15:45 +0100

    Week3 Assignment

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,12 @@ let year = date.getFullYear().toString().padStart(2, 0);
 let day = date.getDate().toString().padStart(2, 0);
 let month = date.getMonth().toString().padStart(2, 0);
 
+function calculateWindChill(T, V) {
+  let windChill = 35.74 + (0.6215 * T) - 35.75 * (V ** 0.16) + 0.4275 * T * (V ** 0.16)
+  return windChill.toFixed(1)
+}
+
+console.log(calculateWindChill(10, 5));
 currentYear.textContent = year;
 
 setInterval(() => {
