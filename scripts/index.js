9bff8993acdddb0860ef24fc8a94fb0a8f70def2
wisdomeffiong2008@gmail.com <wisdomeffiong2008@gmail.com>
2025-07-21 18:01:11 +0100

    Week 3 Assignment

--- scripts/index.js
+++ scripts/index.js
@@ -6,12 +6,6 @@ let year = date.getFullYear().toString().padStart(2, 0);
 let day = date.getDate().toString().padStart(2, 0);
 let month = date.getMonth().toString().padStart(2, 0);
 
-function calculateWindChill(T, V) {
-  let windChill = 35.74 + (0.6215 * T) - 35.75 * (V ** 0.16) + 0.4275 * T * (V ** 0.16)
-  return windChill.toFixed(1)
-}
-
-console.log(calculateWindChill(10, 5));
 currentYear.textContent = year;
 
 setInterval(() => {
@@ -20,3 +14,22 @@ setInterval(() => {
     24
   )}`;
 });
+
+const temperature = 10;
+const windSpeed = 5
+
+function calculateWindChill(T, V) {
+  return (13.12 + (0.6215 * T) - (11.35 * Math.pow(V, 0.16)) + (0.3965 * T * Math.pow(V, 0.16))).toFixed(1);
+}
+
+let windChill;
+
+if (temperature <= 10 && windSpeed > 4.8) {
+  windChill =  calculateWindChill(temperature, windSpeed)
+}
+
+else {
+  windChill =  N/A
+}
+
+console.log(`Wind Chill: ${windChill}`)
