e47de6c162810eb571099cb5f8ad954dc92da9f9
wisdomeffiong2008@gmail.com <wisdomeffiong2008@gmail.com>
2025-07-28 14:47:38 +0100

    Week 4 Assignment

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,192 @@
-let currentYear = document.getElementById("year");
-let lastModified = document.getElementById("modified");
+const temples = [
+  {
+    templeName: "Aba Nigeria",
+    location: "Aba, Nigeria",
+    dedicated: "2005, August, 7",
+    size: "11,500",
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg",
+  },
+  {
+    templeName: "Manti Utah",
+    location: "Manti, Utah, United States",
+    dedicated: "1888, May, 21",
+    size: "74,792",
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg",
+  },
+  {
+    templeName: "Payson Utah",
+    location: "Payson, Utah, United States",
+    dedicated: "2015, June, 7",
+    size: "96,630",
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg",
+  },
+  {
+    templeName: "Yigo Guam",
+    location: "Yigo, Guam",
+    dedicated: "2020, May, 2",
+    size: "6,861",
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg",
+  },
+  {
+    templeName: "Washington D.C.",
+    location: "Kensington, Maryland, United States",
+    dedicated: "1974, November, 19",
+    size: "156,558",
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg",
+  },
+  {
+    templeName: "Lima Perú",
+    location: "Lima, Perú",
+    dedicated: "1986, January, 10",
+    size: "9,600",
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg",
+  },
+  {
+    templeName: "Mexico City Mexico",
+    location: "Mexico City, Mexico",
+    dedicated: "1986, December, 2",
+    size: "1,166,442",
+    imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg",
+  },
+  {
+    templeName: "Cobán Guatemala",
+    location: "Cobán Guatemal",
+    dedicated: "2024, June, 9",
+    size: "8,772",
+    imageUrl: "temple8.webp",
+  },
+  {
+    templeName: "Salt Lake Temple",
+    location: "Salt Lake city, Utah",
+    dedicated: "1893, April, 24",
+    size: "253,015",
+    imageUrl: "temple8.webp",
+  },
+];
+
+let main = document.querySelector("main");
+let templeCont = document.createElement("div");
+
+templeCont.id = "temple-cont";
+main.appendChild(templeCont);
+
+function displayTemples(templesArray) {
+  templeCont.innerHTML = "";
+
+  templesArray.forEach((temple) => {
+    let figure = document.createElement("figure");
+    let figcaption = document.createElement("figcaption");
+    let img = document.createElement("img");
+    let h3 = document.createElement("h3");
+    let location = document.createElement("p");
+    let dedication = document.createElement("p");
+    let size = document.createElement("p");
+
+    img.setAttribute("src", temple.imageUrl);
+    img.setAttribute("alt", temple.templeName);
+    h3.textContent = temple.templeName;
+    location.innerHTML = `<span>Location:</span> ${temple.location}`;
+    dedication.innerHTML = `<span>Dedication:</span> ${temple.dedicated}`;
+    size.innerHTML = `<span>Size:</span> ${temple.size}`;
+
+    figcaption.append(h3, location, dedication, size);
+    figure.append(img, figcaption);
+
+    templeCont.appendChild(figure);
+  });
+}
+
+displayTemples(temples);
+
+let homeBtn = document.getElementById("home");
+homeBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let allTemples = temples.filter((temple) => {
+    return temple;
+  });
+
+  displayTemples(allTemples);
+});
+
+let oldBtn = document.getElementById("old");
+oldBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let oldTemples = temples.filter((temple) => {
+    return parseInt(temple.dedicated.split(",")[0]) < 1900;
+  });
+  displayTemples(oldTemples);
+});
+
+let newBtn = document.getElementById("new");
+newBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let newTemples = temples.filter((temple) => {
+    return parseInt(temple.dedicated.split(",")[0]) > 2000;
+  });
+
+  displayTemples(newTemples);
+});
+
+let largeBtn = document.getElementById("large");
+largeBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let largeTemples = temples.filter((temple) => {
+    return parseInt(temple.size.split(",")[0]) > 90000;
+  });
+
+  displayTemples(largeTemples);
+});
+
+let smallBtn = document.getElementById("small");
+smallBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  let smallTemples = temples.filter((temple) => {
+    return parseInt(temple.size.split(",")[0]) < 10000;
+  });
+
+  displayTemples(smallTemples);
+});
+
+let menuIcon = document.getElementById("menu-icon");
+let navBar = document.getElementById("nav-bar");
+let removeMenu = document.getElementById("remove-menu");
+
+menuIcon.addEventListener("click", () => {
+  navBar.style.display = "flex";
+  main.style.marginTop = "300px";
+  removeMenu.style.display = "block";
+});
+
+removeMenu.addEventListener("click", () => {
+  navBar.style.display = "";
+  main.style.marginTop = "";
+  removeMenu.style.display = "";
+});
 
 let date = new Date();
 let year = date.getFullYear().toString().padStart(2, 0);
 let day = date.getDate().toString().padStart(2, 0);
 let month = date.getMonth().toString().padStart(2, 0);
 
-currentYear.textContent = year;
+document.getElementById("year").textContent = year;
 
 setInterval(() => {
-  lastModified.textContent = `Last Modification: ${month}/${day}/${year}, ${Date().slice(
+  document.getElementById(
+    "last-modified"
+  ).textContent = `Last Modification: ${month}/${day}/${year}, ${Date().slice(
     16,
     24
-  )}`;
+  )}`
 });
-
-const temperature = 10;
-const windSpeed = 5
-
-function calculateWindChill(T, V) {
-  return (13.12 + (0.6215 * T) - (11.35 * Math.pow(V, 0.16)) + (0.3965 * T * Math.pow(V, 0.16))).toFixed(1);
-}
-
-let windChill;
-
-if (temperature <= 10 && windSpeed > 4.8) {
-  windChill =  calculateWindChill(temperature, windSpeed)
-}
-
-else {
-  windChill =  N/A
-}
-
-console.log(`Wind Chill: ${windChill}`)
